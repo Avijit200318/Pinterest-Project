24d77c4c94486c1088be988b444f7fe5861c6550
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-01-23 18:32:08 +0530

    first commit

---
+++ routes/users.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const plm = require("passport-local-mongoose");
+// 
+mongoose.connect("mongodb+srv://avijithira14:yySggpqUSzziOZD9@cluster0.wv0tfyh.mongodb.net/");
+
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  email: String,
+  password: String,
+  profileImage: String,
+  contact: Number,
+  boards: {
+    type: Array,
+    default: [],
+  },
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    }
+  ],
+});
+
+userSchema.plugin(plm);
+
+module.exports = mongoose.model("User", userSchema);
